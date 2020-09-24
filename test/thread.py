01978ace1984c8f982763099cb2e1d9d257ebd6f
zhaopeisheng <zhaops@soyuan.com.cn>
2020-09-24 15:45:24 +0800

    docker 增加mysql 数据库

---
+++ test/thread.py
@@ -0,0 +1,19 @@
+import time
+import thread
+
+
+def print_time(threadName, delay):
+    count = 0
+    while count < 5:
+        time.sleep(delay)
+        count += 1
+        print("%s:%s" % (threadName, time.ctime(time.time())))
+
+
+try:
+    thread.start_new_thread(print_time, ("Thread-1", 2))
+except:
+    print("Error: unable to start thread")
+
+while 1:
+    pass
